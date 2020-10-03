0e68daf9fbb8fd62fd05c0505ab4ba8e43b9dc50
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-03 04:45:12 +0000

    Update docs after building Travis build 207 of OpenGeoVis/omfvista
    The docs were built from the branch 'master' against the commit
    1fdbaa786e7dd632675b34c348925add9b39c521.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/OpenGeoVis/omfvista/jobs/732450222.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs ./docs/_build/html/ .

--- _static/copybutton.js
+++ _static/copybutton.js
@@ -60,19 +60,6 @@ const temporarilyChangeTooltip = (el, newText) => {
   setTimeout(() => el.setAttribute('data-tooltip', oldText), 2000)
 }
 
-// Callback when a copy button is clicked. Will be passed the node that was clicked
-// should then grab the text and replace pieces of text that shouldn't be used in output
-var copyTargetText = (trigger) => {
-  var target = document.querySelector(trigger.attributes['data-clipboard-target'].value);
-  var textContent = target.textContent.split('\n');
-  textContent.forEach((line, index) => {
-    if (line.startsWith(copybuttonSkipText)) {
-      textContent[index] = line.slice(copybuttonSkipText.length)
-    }
-  });
-  return textContent.join('\n')
-}
-
 const addCopyButtonToCodeCells = () => {
   // If ClipboardJS hasn't loaded, wait a bit and try again. This
   // happens because we load ClipboardJS asynchronously.
@@ -94,6 +81,60 @@ const addCopyButtonToCodeCells = () => {
     </a>`
     codeCell.insertAdjacentHTML('afterend', clipboardButton(id))
   })
+
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+// Callback when a copy button is clicked. Will be passed the node that was clicked
+// should then grab the text and replace pieces of text that shouldn't be used in output
+function formatCopyText(textContent, copybuttonPromptText, isRegexp = false, onlyCopyPromptLines = true, removePrompts = true) {
+
+    var regexp;
+    var match;
+
+    // create regexp to capture prompt and remaining line
+    if (isRegexp) {
+        regexp = new RegExp('^(' + copybuttonPromptText + ')(.*)')
+    } else {
+        regexp = new RegExp('^(' + escapeRegExp(copybuttonPromptText) + ')(.*)')
+    }
+
+    const outputLines = [];
+    var promptFound = false;
+    for (const line of textContent.split('\n')) {
+        match = line.match(regexp)
+        if (match) {
+            promptFound = true
+            if (removePrompts) {
+                outputLines.push(match[2])
+            } else {
+                outputLines.push(line)
+            }
+        } else {
+            if (!onlyCopyPromptLines) {
+                outputLines.push(line)
+            }
+        }
+    }
+
+    // If no lines with the prompt were found then just use original lines
+    if (promptFound) {
+        textContent = outputLines.join('\n');
+    }
+
+    // Remove a trailing newline to avoid auto-running when pasting
+    if (textContent.endsWith("\n")) {
+        textContent = textContent.slice(0, -1)
+    }
+    return textContent
+}
+
+
+var copyTargetText = (trigger) => {
+  var target = document.querySelector(trigger.attributes['data-clipboard-target'].value);
+  return formatCopyText(target.innerText, '', false, true,  true)
+}
 
   // Initialize with a callback so we can modify the text before copy
   const clipboard = new ClipboardJS('.copybtn', {text: copyTargetText})
